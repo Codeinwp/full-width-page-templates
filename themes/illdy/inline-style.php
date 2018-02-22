190afdf866f95df882707676a2dcea89ec627dc5
coti <stefan@themeisle.com>
2018-02-22 17:58:56 +0200

    Compatibility for thew following themes added: Hestia Zerif Lite Kale GeneratePress Spacious Flash Writee ShopIsle VT Blogging One Page Express Primer Vantage Customizr Nikko Portfolio Poseidon Envo Business Hueman News Portal Illdy Envy Blog Avant

---
+++ themes/illdy/inline-style.php
@@ -0,0 +1,8 @@
+<?php
+/* Support for the Illdy theme */
+$illdy = '
+		.page-template-builder-fullwidth-std {
+			
+		}
+	';
+wp_add_inline_style( 'illdy-style', $illdy );
--- themes/zerif-lite/inline-style.php
+++ themes/zerif-lite/inline-style.php
@@ -1,20 +1,13 @@
 <?php
 /* Support for the Zerif Lite theme */
-//$virtue = '.page-template-builder-fullwidth .headerclass,
-//.page-template-builder-fullwidth .footerclass {
-//    display: none;
-//}
-//.page-template-builder-fullwidth .contentclass,
-//.page-template-builder-fullwidth-std .contentclass {
-//    padding-bottom: 0;
-//}
-//.page-template-builder-fullwidth .contentclass {
-//    padding-top: 0;
-//}';
-//wp_add_inline_style( 'kadence_theme', $virtue );
-//
-//function zerif_close_header() {
-//	echo '</header> <!-- / END HOME SECTION  -->';
-//}
-//add_action( 'fwpt_std_before_content', 'zerif_close_header' );
-//add_action( 'fwpt_blank_before_content', 'zerif_close_header' );
+$style = '
+		.page-template-builder-fullwidth {
+			overflow: hidden;
+		}
+		@media (min-width: 768px) {
+			.page-template-builder-fullwidth-std .header > .elementor {
+				padding-top: 76px;
+			}
+		}
+';
+wp_add_inline_style( 'zerif_style', $style );
